55bcdcefa8759168e57d48d79ee892c40cbdccaa
Gabriel Estevam <gabriel.estevam.narciso@gmail.com>
2021-09-07 23:11:45 -0300

    Corrigir operação de incrementacao

--- analisador-sintatico/codigos/Prob2/main.m
+++ analisador-sintatico/codigos/Prob2/main.m
@@ -19,13 +19,13 @@ void main() {
         scanf("%d", &n);
 
         if (n >= 0 && n <= 25) {
-            inter1++;
+            inter1 = inter1 + 1;
         }else if (n >= 26 && n <= 50) {
-            inter2++;
+            inter2 = inter2 + 1;
         }else if (n >= 51 && n <= 75) {
-            inter3++;
+            inter3 = inter3 + 1;
         }else if (n >= 76 && n <= 100) {
-            inter4++;
+            inter4 = inter4 + 1;;
         }
 
     }
